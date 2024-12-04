9271f49911d4f76a449277d37d2f0a0a2f08c3d6
Kayque Goldner <kayquegoldner@gmail.com>
2024-12-04 08:11:39 -0300

    landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,15 @@
+import LandingPage from "@/app/(nondashboard)/landing/page";
+import { Footer } from "@/components/Footer";
+import { NonDashboardNavbar } from "@/components/NonDashboardNavbar";
+
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <LandingPage />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="">&copy; ByteBloom. All Rights Reserved.</p>
+      <div className="footer__links">
+        {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
+          <Link
+            key={item}
+            href={`/${item.toLowerCase().replace(" ", "-")}`}
+            className="footer__link"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
